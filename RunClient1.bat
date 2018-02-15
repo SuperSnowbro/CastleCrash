7d378d0a7a59936da774ced6eba05462db2ef73d
eugenio.delvecchio <esnow.dv@gmail.com>
2018-02-15 16:12:46 +0100

    Cube amount limit implemented

--- RunClient1.bat
+++ RunClient1.bat
@@ -1,2 +1,1 @@
- TIMEOUT /T 10
- "C:\UE4Source\MPCustom\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\RV10Rebuild.uproject" 127.0.0.1 -game -log -nosteam
+ "C:\Program Files\Epic Games\UE_4.18\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\CastleCrash.uproject" Untitled -game -log -nosteam
